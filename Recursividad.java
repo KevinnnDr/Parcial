a8ca7579798d70817434b13d12c856736377eb0b
Kevin Esteban Rodriguez Guerrero <kestebanrodriguez@ucompensar.edu.co>
2026-03-09 19:41:15 -0500

    recursividad

---
+++ Recursividad.java
@@ -0,0 +1,16 @@
+public class Recursividad {
+    public static String invertirCadena(String texto) {
+        if (texto.length() <= 1) {
+            return texto;
+        }
+
+        return invertirCadena(texto.substring(1)) + texto.charAt(0);
+    }
+ 
+    public static void main(String[] args) {
+        String original = "Recursividad";
+        String invertida = invertirCadena(original);
+        System.out.println("Original: " + original);
+        System.out.println("Invertida: " + invertida);
+    }
+}
